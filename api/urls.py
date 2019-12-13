83d6fef46cb0f9e49db6600f8b86e44d3a6909dd
Govind Thakur <govind.thakur11@gmail.com>
2019-12-13 15:12:33 +0530

    Near Final stages, having a permission issue in routers

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 #from .models import BooksConfig
+from .views import BookViewSet,UserViewSet
 from .views import BookAPIView,BookDetail,UserList,UserDetail
-
+from rest_framework.routers import SimpleRouter #THere are 2 types, were using
+#the simple one here
+"""
+------------- Using Routers instead of the code below ------------------------
+"""
 urlpatterns = [
 
     path('', BookAPIView.as_view()),
@@ -11,3 +16,11 @@ urlpatterns = [
     path('users/<int:pk>/',UserDetail.as_view())
 
 ]
+"""
+---------------ROUTER CODE: NEEDS WORK--------------
+router = SimpleRouter()
+router.register('users', UserViewSet, base_name = 'users')
+router.register('', BookViewSet, base_name='books')
+
+urlpatterns = router.urls
+"""
--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model #user authentication
-# Create your views here.
-from rest_framework import generics
+# Create your views here.]
+from rest_framework import generics, viewsets
+from .permissions import IsAuthorOrReadOnly #permissions
 from books.models import Book
 from .serializers import BookSerializer,UserSerializer #the serializer classes created
 #in serializers.py
+
+"""
+---------This code is for defualt views, and will br replaced by Viewsets-----------------
+"""
 '''CRUDing BOOKS'''
 class BookAPIView(generics.ListCreateAPIView):#List of all the books
     queryset = Book.objects.all()
@@ -22,5 +27,16 @@ class UserList(generics.ListCreateAPIView):
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+"""
+----------------------Needs work (ROUTERS)--------------------------
+"""
+class BookViewSet (viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class UserViewSet(viewsets.ModelViewSet): # new
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
